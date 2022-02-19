215006fdc10dcd93de0fb7f8ad1323ca1100cdb7
akaanug <46591206+akaanug@users.noreply.github.com>
2022-02-19 10:02:50 +0300

    boto fix

--- backend/settings.py
+++ backend/settings.py
@@ -205,8 +205,10 @@ AWS_SECRET_ACCESS_KEY = os.environ["SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = "shopcart-senior"
 AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
+AWS_S3_REGION_NAME = "eu-central-1"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
 
 '''
 <?xml version="1.0" encoding="UTF-8"?>
