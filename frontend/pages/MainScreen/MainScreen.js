d16097544a63018e7cb54e50d5097436d8669190
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-03-11 19:58:15 +0300

    auth - redirect to initial screen if not log in

--- frontend/pages/MainScreen/MainScreen.js
+++ frontend/pages/MainScreen/MainScreen.js
@@ -1,39 +1,55 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
-import LoginScreen from '../LoginScreen/LoginScreen';
-import HomeScreen from '../HomeScreen/HomeScreen';
 import SignUpScreen from '../SignUpScreen/SignUpScreen';
 import BarcodeScanner from '../BarcodeScanner/BarcodeScanner';
 import ShoppingListScreen from '../ShoppingListScreen/ShoppingListScreen';
 import { userToken } from '../../store/slices/token';
 import ApprovalScreen from '../BarcodeScanner/ApprovalScreen';
+import InitialScreen from '../InitialScreen/InitialScreen';
+import ShopCartBottomTabs from '../TabNavigator/TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 const MainScreen = () => {
   const isSignedIn = useSelector(userToken);
-  console.log('this is issigned in', isSignedIn)
   return (
     <Stack.Navigator>
-      {
-        isSignedIn ? (
-          <>
-            <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
-            <Stack.Screen options={{ headerShown: false }} name="Add Product via Barcode" component={BarcodeScanner} />
-            <Stack.Screen options={{ headerShown: false }} name="Shopping List" component={ShoppingListScreen} />
-            <Stack.Screen options={{ headerShown: false }} name="Approval" component={ApprovalScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-            <Stack.Screen options={{ headerShown: false }} name="Sign Up" component={SignUpScreen} />
-          </>
-        )
-      }
+      {isSignedIn ? (
+        <>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Tab Navigator"
+            component={ShopCartBottomTabs}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Add Product via Barcode"
+            component={BarcodeScanner}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Shopping List"
+            component={ShoppingListScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Approval"
+            component={ApprovalScreen}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Initial Screen"
+            component={InitialScreen}
+          />
+          <Stack.Screen options={{ headerShown: false }} name="Sign Up" component={SignUpScreen} />
+        </>
+      )}
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default MainScreen
-
+export default MainScreen;
