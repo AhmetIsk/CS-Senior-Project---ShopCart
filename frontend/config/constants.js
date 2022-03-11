9b08b1d4e52c8f1c6c558c242f8f64e189dd1cbe
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-03-11 19:51:56 +0300

    add new constants and fix api url

--- frontend/config/constants.js
+++ frontend/config/constants.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const API_URL = 'https://shopcart-s.herokuapp.com/';
+export const API_URL = 'https://shopcart-s.herokuapp.com';
--- frontend/constants/styles.js
+++ frontend/constants/styles.js
@@ -1,32 +1,40 @@
 export const fontStyles = {
   Arial: 'Roboto',
   Andale: 'Andale Mono, monospace',
-  Lucida: '"Lucida Console", Courier, monospace'
-}
+  Lucida: '"Lucida Console", Courier, monospace',
+};
 
 export const colors = {
+  disabledText: '#9ca3af',
   gray: 'rgb(150, 150, 150)',
   headerRed: 'rgb(198, 21, 21)',
   white: '#FFFFFF',
   black: '#000',
   green: '#41A426',
   softGreen: '#70e45a',
-  blue:'#1C5AC7',
-  purple:'#A020F0',
-}
+  blue: '#1C5AC7',
+  purple: '#A020F0',
+  orange: '#f25b05',
+  fadeOrange: '#f9ae83',
+  subtitle: '#4b5563',
+  title: '#1f2937',
+  termscond: '#242323',
+  borderColor: '#bec5d1',
+  disabled: '#f4f4f4',
+};
 
 export const fontWeights = {
   thin: 200,
   normal: 500,
-  bold: 700
-}
+  bold: 700,
+};
 
 export const fontSizes = {
   small: 16,
   medium: 20,
-  large: 24
-}
+  large: 24,
+};
 
 export const radius = {
-  normal: 10
-}
+  normal: 10,
+};
