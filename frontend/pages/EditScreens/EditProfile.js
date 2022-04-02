83ea7e5c8fce847c82daae7d91f206a0359921a9
Mehmet Yaylacı <mehmet.yaylaci@ug.bilkent.edu.tr>
2022-04-03 00:49:25 +0300

    editpage first iteration

--- frontend/pages/EditScreens/EditProfile.js
+++ frontend/pages/EditScreens/EditProfile.js
@@ -1,12 +1,51 @@
-import { View, Text, KeyboardAvoidingView } from 'react-native'
-import React from 'react'
-import { styles } from '../HomeScreen/styles'
+import { View, Text, KeyboardAvoidingView, TextInput, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { styles } from './styles';
+import LayoutSVG from '../../assets/layout.svg';
+import { ButtonLabel, SettingsButtons } from '../../components/Buttons';
+import { ButtonContainer } from '../../components/Containers';
+import { colors } from '../../constants/styles';
+import { useDispatch } from 'react-redux';
 
 const EditProfile = () => {
+  const dispatch = useDispatch();
+  const [FirstName, setFirstName] = useState('');
+  const [LastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    console.log("Submitted")
+  };
+
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <Text>EditProfile</Text>
-    </KeyboardAvoidingView>
+    <View style={styles.container} behavior="padding">
+      <LayoutSVG width={414} height={245} style={{ position: 'absolute', top: 0, zIndex: 2 }} />
+      <View style={styles.settingsContainer}>
+          <View style={styles.inputsContainer}>
+            <Text style={styles.inputLabel}>FirstName</Text>
+            <TextInput
+              placeholder="Ravan"
+              value={FirstName}
+              onChangeText={(text) => {setFirstName(text)}}
+              style={styles.input}
+            />
+            <Text style={styles.inputLabel}>LastName</Text>
+            <TextInput
+              placeholder="Aliyev"
+              value={LastName}
+              onChangeText={(text) => {setLastName(text)}}
+              style={styles.input}
+            />
+            <Text style={styles.inputLabel}>Email</Text>
+            <TextInput
+              placeholder="mehmetyaylaci2011@hotmail.com"
+              value={email}
+              onChangeText={(text) => {setEmail(text)}}
+              style={styles.input}
+            />
+          </View>
+      </View>
+    </View>
   )
 }
 
