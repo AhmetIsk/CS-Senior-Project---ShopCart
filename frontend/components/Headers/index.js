d294885d7c7d58125ff37bf8bae92aa8e72b23de
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-04-10 11:58:01 +0300

    eslint update

--- frontend/components/Headers/index.js
+++ frontend/components/Headers/index.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
-import { colors } from "../../constants/styles";
+import styled from 'styled-components';
+import { colors } from '../../constants/styles';
 
 export const MyShoppingList = styled.Text`
-color: ${colors.headerRed};
-font-weight: 700;
-font-size: 24px;
-`
+  color: ${colors.headerRed};
+  font-weight: 700;
+  font-size: 24px;
+`;
