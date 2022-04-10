eeeac48f4b08275191626c3f8fa45aba4fe46c01
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-04-10 12:00:23 +0300

    create add shoplist screen

--- frontend/pages/MyListsScreen/AddShoppingListScreen.js
+++ frontend/pages/MyListsScreen/AddShoppingListScreen.js
@@ -3,91 +3,129 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react';
-import { KeyboardAvoidingView, Linking, Text, TextInput, View } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { KeyboardAvoidingView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { Checkbox } from 'react-native-paper';
-import SVGoogle from '../../assets/icGoogle.svg';
-import { styles } from '../LoginScreen/styles';
-import { login } from '../../store/actions/auth';
-import { ButtonLabel, StyledSignInUpButton } from '../../components/Buttons';
+import { Entypo } from '@expo/vector-icons';
+import { styles } from './styles';
+import { inputStyles } from './styles/inputStyle';
 import { getAuthAsyncStorage } from '../../services/getAuthAsyncStorage';
-import { setToken } from '../../store/slices/token';
+import { setToken, userToken } from '../../store/slices/token';
 import { colors } from '../../constants/styles';
+import PriorityButton from '../../components/Buttons/radioButton';
+import { userService } from '../../services/userService';
 
 // TODO: Input alinacak kisimlar yeni bir child componentte verilecek rerender sayisini azaltmak icin
-const AddShoppingListScreen = () => {
+const AddShoppingListScreen = ({ navigation }) => {
+  const token = useSelector(userToken);
   const dispatch = useDispatch();
   const [listName, setListName] = useState('');
   const [community, setCommunity] = useState('');
   const [checked, setChecked] = useState(false);
+  const [priority, setPriority] = useState('');
 
   useEffect(() => {
     const load = async () => {
       const userStorage = await getAuthAsyncStorage();
-      console.log('bu user storage homedaki', userStorage);
       dispatch(setToken(userStorage.token));
     };
     load();
   }, []);
 
   const handleSubmit = () => {
-    login(listName, community, dispatch);
+    userService.addShoppingList(listName, priority, token);
     setListName('');
     setCommunity('');
+    setPriority('');
+    setChecked(false);
+  };
+
+  const discardChanges = () => {
+    setListName('');
+    setCommunity('');
+    setPriority('');
+    setChecked(false);
   };
 
   return (
     <>
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputLabel}>Add List Name</Text>
+      <KeyboardAvoidingView style={inputStyles.container}>
+        <View style={styles.header}>
+          <Entypo
+            name="chevron-thin-left"
+            size={26}
+            color={colors.white}
+            style={{ padding: 8 }}
+            onPress={() => navigation.goBack()}
+          />
+          <Text style={styles.headerTitle}>My Lists</Text>
+          <Text style={{ width: 33 }} />
+        </View>
+        <View style={inputStyles.inputContainer}>
+          <Text style={inputStyles.inputLabel}>Add List Name</Text>
           <TextInput
-            placeholder="Eg. AhmetIsk"
+            placeholder="Eg. Weekend List"
             value={listName}
             onChangeText={(text) => setListName(text)}
-            style={styles.input}
+            style={inputStyles.input}
           />
-          <Text style={styles.inputLabel}>Add to Community</Text>
+          <Text style={inputStyles.inputLabel}>Communities</Text>
           <TextInput
-            placeholder="*** **** ***"
+            placeholder="Add new community"
             value={community}
             onChangeText={(text) => setCommunity(text)}
-            style={styles.input}
+            style={inputStyles.input}
             secureTextEntry
           />
-          <View style={styles.checkbox}>
-            <Checkbox
-              status={checked ? 'checked' : 'unchecked'}
-              onPress={() => {
-                setChecked(!checked);
-              }}
-              color="red"
-            />
-            <Text style={styles.inputLabel}>Make it default</Text>
+          <View style={inputStyles.checkbox}>
+            <View style={inputStyles.checkboxBackground}>
+              <Checkbox
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={() => {
+                  setChecked(!checked);
+                }}
+                color="red"
+              />
+            </View>
+            <Text style={inputStyles.inputLabel}>Make it default</Text>
           </View>
-          <Text style={styles.forgotPass} onPress={() => Linking.openURL('http://google.com')}>
-            Forgot Community?
-          </Text>
         </View>
-        <View style={styles.registrationContainer}>
-          <StyledSignInUpButton
-            onPress={handleSubmit}
-            color={listName && community ? colors.orange : colors.disabled}
-            disabled={!(listName && community)}
-          >
-            <ButtonLabel color={listName && community ? colors.white : colors.disabledText}>
-              Login
-            </ButtonLabel>
-          </StyledSignInUpButton>
-          <StyledSignInUpButton
-            onPress={handleSubmit}
-            color={listName && community ? colors.orange : colors.disabled}
-          >
-            <SVGoogle width={20} height={20} />
-            <ButtonLabel color={listName && community ? colors.white : colors.disabledText}>
-              Login with Google
-            </ButtonLabel>
-          </StyledSignInUpButton>
+        <View>
+          <Text style={inputStyles.inputLabel}>Set Priority</Text>
+          <View style={inputStyles.priorityContainer}>
+            <View style={inputStyles.radioContainer}>
+              <PriorityButton
+                priority="high"
+                onPress={() => setPriority('High')}
+                selected={priority}
+              />
+              <Text style={inputStyles.informalText}>High</Text>
+            </View>
+            <View style={inputStyles.radioContainer}>
+              <PriorityButton
+                priority="medium"
+                onPress={() => setPriority('Medium')}
+                selected={priority}
+              />
+              <Text style={inputStyles.informalText}>Medium</Text>
+            </View>
+            <View style={inputStyles.radioContainer}>
+              <PriorityButton
+                priority="low"
+                onPress={() => setPriority('Low')}
+                selected={priority}
+              />
+              <Text style={inputStyles.informalText}>Low</Text>
+            </View>
+          </View>
+        </View>
+        <View style={inputStyles.buttonContainer}>
+          <TouchableOpacity style={styles.addListButton} onPress={handleSubmit}>
+            <Text style={styles.addListLabel}>Add List</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.discardChanges} onPress={discardChanges}>
+            <Text style={inputStyles.discardChanges}>Discard Changes</Text>
+          </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
     </>
