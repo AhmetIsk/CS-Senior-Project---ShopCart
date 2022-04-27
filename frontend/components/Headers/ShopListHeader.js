e85b6a906abb2ae7807c795082734a693712f45b
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-04-28 00:07:42 +0300

    modify shop list header

--- frontend/components/Headers/ShopListHeader.js
+++ frontend/components/Headers/ShopListHeader.js
@@ -1,13 +1,21 @@
 /* eslint-disable react/prop-types */
-import { StyleSheet, Text } from 'react-native';
-import React from 'react';
-import { Entypo } from '@expo/vector-icons';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Alert, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { AntDesign, Entypo } from '@expo/vector-icons';
+import { useSelector } from 'react-redux';
 import HighBackground from '../../assets/HighPriBackground.svg';
 import MediumBackground from '../../assets/MediumPriBackground.svg';
 import LowBackground from '../../assets/LowPriBackground.svg';
 import { colors } from '../../constants/styles';
+import { userService } from '../../services/userService';
+import { userToken } from '../../store/slices/token';
+import { shopListId } from '../../store/slices/shopListId';
 
 export default function ShopListHeader({ priority, listName, navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newName, setNewName] = useState(listName);
+  const token = useSelector(userToken);
+  const id = useSelector(shopListId);
   if (priority === 'Low') {
     return (
       <>
@@ -19,7 +27,52 @@ export default function ShopListHeader({ priority, listName, navigation }) {
           onPress={() => navigation.goBack()}
         />
         <LowBackground />
-        <Text style={styles.listName}>{listName}</Text>
+        <Modal
+          animationType="slide"
+          transparent
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <AntDesign name="closecircle" size={30} color={`${colors.orange}`} />
+              </TouchableOpacity>
+              <Text style={styles.changeNameHeader}>Edit the name below:</Text>
+              <TextInput
+                value={newName}
+                style={styles.input}
+                onChangeText={(text) => {
+                  setNewName(text);
+                }}
+              />
+              <TouchableOpacity
+                style={styles.buttonClose}
+                onPress={() => {
+                  userService
+                    .changeShopListName(id, token, newName)
+                    .then(() => setModalVisible(!modalVisible));
+                }}
+              >
+                <Text style={styles.textStyle}>Change List Name</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+        <View style={styles.textContainer}>
+          <View style={styles.inlineTextContainer}>
+            <Text style={styles.listName}>{listName}</Text>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+              <Entypo name="edit" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
       </>
     );
   }
@@ -34,7 +87,48 @@ export default function ShopListHeader({ priority, listName, navigation }) {
           onPress={() => navigation.goBack()}
         />
         <MediumBackground />
-        <Text style={styles.listName}>{listName}</Text>
+        <Modal
+          animationType="slide"
+          transparent
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <AntDesign name="closecircle" size={30} color={`${colors.orange}`} />
+              </TouchableOpacity>
+              <Text style={styles.changeNameHeader}>Edit the name below:</Text>
+              <TextInput
+                value={newName}
+                style={styles.input}
+                onChangeText={(text) => {
+                  setNewName(text);
+                }}
+              />
+              <TouchableOpacity
+                style={styles.buttonClose}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Change List Name</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+        <View style={styles.textContainer}>
+          <View style={styles.inlineTextContainer}>
+            <Text style={styles.listName}>{listName}</Text>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+              <Entypo name="edit" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
       </>
     );
   }
@@ -49,23 +143,137 @@ export default function ShopListHeader({ priority, listName, navigation }) {
           onPress={() => navigation.goBack()}
         />
         <HighBackground />
-        <Text style={styles.listName}>{listName}</Text>
+        <Modal
+          animationType="slide"
+          transparent
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <AntDesign name="closecircle" size={30} color={`${colors.orange}`} />
+              </TouchableOpacity>
+              <Text style={styles.changeNameHeader}>Edit the name below:</Text>
+              <TextInput
+                value={newName}
+                style={styles.input}
+                onChangeText={(text) => {
+                  setNewName(text);
+                }}
+              />
+              <TouchableOpacity
+                style={styles.buttonClose}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Change List Name</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+        <View style={styles.textContainer}>
+          <View style={styles.inlineTextContainer}>
+            <Text style={styles.listName}>{listName}</Text>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+              <Entypo name="edit" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
       </>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  listName: {
+  closeButton: {
     position: 'absolute',
-    top: 150,
-    left: 50,
-    zIndex: 1,
+    top: 5,
+    right: 5,
+  },
+  input: {
+    backgroundColor: 'white',
+    width: 200,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: `${colors.borderColor}`,
+    marginTop: 10,
+  },
+  listName: {
     fontSize: 30,
     fontWeight: '600',
     fontStyle: 'normal',
     lineHeight: 41,
     letterSpacing: 0.36,
     color: `${colors.white}`,
+  },
+  textContainer: {
+    position: 'absolute',
+    top: 150,
+    left: 50,
+    zIndex: 1,
+    width: 300,
+  },
+  inlineTextContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    width: 200,
+    height: 50,
+    borderRadius: 12,
+    justifyContent: 'center',
+    backgroundColor: `${colors.orange}`,
+    margin: 20,
+  },
+  textStyle: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  changeNameHeader: {
+    fontSize: 20,
+    paddingBottom: 20,
+    color: `${colors.orange}`,
   },
 });
