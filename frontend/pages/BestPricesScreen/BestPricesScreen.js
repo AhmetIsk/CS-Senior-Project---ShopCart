ae46579ec7e36a71a1553e88dd280c63f69a3502
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-04-28 00:04:41 +0300

    create best prices screen

---
+++ frontend/pages/BestPricesScreen/BestPricesScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function BestPricesScreen() {
+  return (
+    <View>
+      <Text>BestPricesScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
