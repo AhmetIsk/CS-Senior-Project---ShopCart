6f311a0ad0edf3fe55fba5a0668814e4af2b68c0
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-04-28 00:07:01 +0300

    improve home screen

--- frontend/pages/HomeScreen/HomeScreen.js
+++ frontend/pages/HomeScreen/HomeScreen.js
@@ -1,21 +1,10 @@
 /* eslint-disable global-require */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Text, KeyboardAvoidingView, Image } from 'react-native';
+import { Text, KeyboardAvoidingView, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import {
-  AddProductButton,
-  ButtonLabel,
-  EditButton,
-  EditButtonLabel,
-  ShoppingListButton,
-  SignOut,
-} from '../../components/Buttons';
 import { ButtonContainer } from '../../components/Containers';
-import { colors } from '../../constants/styles';
 import SVGImg from '../../assets/logo.svg';
-import { logout } from '../../store/actions/auth';
 import { styles } from './styles';
 import { userInfoToken } from '../../store/slices/user';
 
@@ -25,6 +14,9 @@ const HomeScreen = ({ navigation }) => {
   console.log('This is user info', userInfo);
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>ShopCart</Text>
+      </View>
       <SVGImg width={118} height={134} />
       <Text style={styles.welcomePage}>Welcome, {userInfo?.first_name} !</Text>
       <ButtonContainer />
