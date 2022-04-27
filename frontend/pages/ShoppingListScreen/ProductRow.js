982804086d5dbf9c95a0b0945ef80b5aaee01c00
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-04-28 00:07:28 +0300

    change styling and functioality of shopping list screen

--- frontend/pages/ShoppingListScreen/ProductRow.js
+++ frontend/pages/ShoppingListScreen/ProductRow.js
@@ -1,52 +1,109 @@
 /* eslint-disable global-require */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, Text, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import { DataTable } from 'react-native-paper';
 import { userService } from '../../services/userService';
 import { userToken } from '../../store/slices/token';
 import { DecreaseProduct } from '../../components/Buttons';
+import { colors } from '../../constants/styles';
+import ProductButtons from '../../assets/productButtons.svg';
 
 // TODO: bu ard arda api req yollamak sikintiliydi. onun cozumune bak; ek olarak trigger seri basmalarda ise yaramiyor cozumu budur belki; degilse shopliste tasi
-export default function ProductRow({ name, quantity, barcode, trigger, value }) {
+export default function ProductRow({
+  name,
+  quantity,
+  barcode,
+  trigger,
+  value,
+  imageUrl,
+  bestPrice,
+}) {
   const token = useSelector(userToken);
   return (
-    <DataTable.Row>
-      <DataTable.Cell style={{ width: 300 }}>{name}</DataTable.Cell>
-      <DataTable.Cell centered numeric>
-        <DecreaseProduct
-          onPress={() => {
-            userService.addProduct(barcode, 1, token);
-            trigger(!value);
-          }}
-        >
-          <Image source={require('../../images/add.png')} style={{ width: 30, height: 30 }} />
-        </DecreaseProduct>
-      </DataTable.Cell>
-      <DataTable.Cell centered numeric>
-        {quantity}
-      </DataTable.Cell>
-      <DataTable.Cell centered numeric>
-        <DecreaseProduct
-          onPress={() => {
-            userService.removeFromList(barcode, 1, token);
-            trigger(!value);
-          }}
-        >
-          <Image source={require('../../images/buy.png')} style={{ width: 30, height: 30 }} />
-        </DecreaseProduct>
-      </DataTable.Cell>
-      <DataTable.Cell centered numeric>
-        <DecreaseProduct
-          onPress={() => {
-            userService.removeFromList(barcode, 1, token);
-            trigger(!value);
-          }}
-        >
-          <Image source={require('../../images/NN.jpg')} style={{ width: 30, height: 30 }} />
-        </DecreaseProduct>
-      </DataTable.Cell>
-    </DataTable.Row>
+    <View style={styles.itemContainer}>
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: imageUrl }} style={{ width: 84, height: 84, margin: 5 }} />
+        <View style={styles.infoContainer}>
+          <Text style={styles.textStyle}>{name}</Text>
+          <Text style={styles.textStyle}>Quantity: {quantity}</Text>
+          <Text>{bestPrice[0]?.name}</Text>
+        </View>
+      </View>
+
+      <ProductButtons />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    width: 327,
+    height: 100,
+    borderRadius: 4,
+    backgroundColor: `${colors.white}`,
+    shadowColor: `${colors.shadowColor}`,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 7,
+    shadowOpacity: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  imageContainer: {
+    flexDirection: 'row',
+  },
+  infoContainer: {
+    padding: 8,
+  },
+  textStyle: {
+    fontSize: 14,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    lineHeight: 16,
+    letterSpacing: -0.39,
+    color: `${colors.textColor}`,
+    paddingTop: 10,
+  },
+});
+// <DataTable.Row>
+//   <DataTable.Cell style={{ width: 300 }}>{name}</DataTable.Cell>
+//   <DataTable.Cell centered numeric>
+//     <DecreaseProduct
+//       onPress={() => {
+//         userService.addProduct(barcode, 1, token);
+//         trigger(!value);
+//       }}
+//     >
+//       <Image source={require('../../images/add.png')} style={{ width: 30, height: 30 }} />
+//     </DecreaseProduct>
+//   </DataTable.Cell>
+//   <DataTable.Cell centered numeric>
+//     {quantity}
+//   </DataTable.Cell>
+//   <DataTable.Cell centered numeric>
+//     <DecreaseProduct
+//       onPress={() => {
+//         userService.removeFromList(barcode, 1, token);
+//         trigger(!value);
+//       }}
+//     >
+//       <Image source={require('../../images/buy.png')} style={{ width: 30, height: 30 }} />
+//     </DecreaseProduct>
+//   </DataTable.Cell>
+//   <DataTable.Cell centered numeric>
+//     <DecreaseProduct
+//       onPress={() => {
+//         userService.removeFromList(barcode, 1, token);
+//         trigger(!value);
+//       }}
+//     >
+//       <Image source={require('../../images/NN.jpg')} style={{ width: 30, height: 30 }} />
+//     </DecreaseProduct>
+//   </DataTable.Cell>
+// </DataTable.Row>
--- frontend/pages/ShoppingListScreen/ShoppingListScreen.js
+++ frontend/pages/ShoppingListScreen/ShoppingListScreen.js
@@ -1,59 +1,81 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-console */
 /* eslint-disable global-require */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from 'react';
-import { Image, Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { DataTable } from 'react-native-paper';
-import { Entypo } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 import { userService } from '../../services/userService';
 import { userToken } from '../../store/slices/token';
 import { styles } from './styles/index';
 import ProductRow from './ProductRow';
 import { MyShoppingList } from '../../components/Headers';
-import { GoBack } from '../../components/Buttons';
-import NOLists from '../../assets/noLists.svg';
+import ListIsEmptyImage from '../../assets/listIsEmpty.svg';
+import ShopListHeader from '../../components/Headers/ShopListHeader';
+import { setId } from '../../store/slices/shopListId';
 import { colors } from '../../constants/styles';
+import AddProductButton from '../../components/Buttons/AddProductButton';
+import BestPricesButton from '../../components/Buttons/BestPricesButton';
 
-const ShoppingListScreen = ({ navigation }) => {
+const ShoppingListScreen = ({ route, navigation }) => {
   const [items, setItems] = useState([]);
+  const { itemId, listName, priority } = route.params;
+  console.log(itemId, 'this is idnum');
+  const dispatch = useDispatch();
+  dispatch(setId(itemId));
   const token = useSelector(userToken);
   const [rerender, setRerender] = useState(false);
   const ListProducts = () => {
-    userService.getShoppingList(token).then((products) => {
+    userService.getShoppingList(itemId, token).then((products) => {
       console.log(products.length);
-      const mappedItems = products.map((product) => (
-        <ProductRow
-          key={product.id}
-          name={product.product.name}
-          quantity={product.quantity}
-          barcode={product.product.barcode}
-          trigger={setRerender}
-          value={rerender}
-        />
-      ));
-      setItems(mappedItems);
+      if (products.length !== 0) {
+        console.log(products);
+        const mappedItems = products.map((product) => (
+          <ProductRow
+            key={product.id}
+            name={product.product.name}
+            quantity={product.quantity}
+            barcode={product.product.barcode}
+            trigger={setRerender}
+            value={rerender}
+            imageUrl={product.product.external_photo_url}
+            bestPrice={product.product.store_set}
+          />
+        ));
+        setItems(mappedItems);
+      }
     });
   };
   useEffect(() => {
+    console.log(itemId, 'this is idnum i nsh');
     ListProducts();
   }, [rerender]);
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Entypo
-          name="chevron-thin-left"
-          size={26}
-          color={colors.white}
-          style={{ padding: 8 }}
-          onPress={() => navigation.navigate('Feed')}
-        />
-        <Text style={styles.headerTitle}>My Lists</Text>
-        <Text style={{ width: 33 }} />
-      </View>
-      <NOLists />
-      <MyShoppingList>My Shopping List</MyShoppingList>
+      <ShopListHeader priority={priority} listName={listName} navigation={navigation} />
+      {items.length === 0 ? (
+        <View style={styles.contentContainer}>
+          <ListIsEmptyImage />
+          <TouchableOpacity
+            style={styles.addProduct}
+            onPress={() => navigation.navigate('Add New Product')}
+          >
+            <Text style={styles.addProductLabel}>Add Product</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={styles.contentContainer}>
+          <ScrollView style={styles.itemsContainer}>{items}</ScrollView>
+          <View style={styles.buttonContainer}>
+            <AddProductButton onPress={() => navigation.navigate('Add New Product')} />
+            <BestPricesButton onPress={() => navigation.navigate('Best Prices')} />
+          </View>
+        </View>
+      )}
+      {/* <MyShoppingList>My Shopping List</MyShoppingList>
       <DataTable>
         <DataTable.Header>
           <DataTable.Title>Product Name</DataTable.Title>
@@ -65,7 +87,7 @@ const ShoppingListScreen = ({ navigation }) => {
           <DataTable.Title numeric>NN</DataTable.Title>
         </DataTable.Header>
         {items}
-      </DataTable>
+      </DataTable> */}
     </View>
   );
 };
