10ec2d632336d60ed1f83afd8f5afe8308eb8ed0
Ahmet Işık <48672998+AhmetIsk@users.noreply.github.com>
2022-04-28 16:57:11 +0300

    add input style

---
+++ frontend/components/Containers/styles/inputStyle.js
@@ -0,0 +1,80 @@
+/* eslint-disable import/prefer-default-export */
+import { StyleSheet } from 'react-native';
+import { colors } from '../../../constants/styles';
+
+export const inputStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: `${colors.white}`,
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderColor: `${colors.inputBorder}`,
+    marginTop: 10,
+  },
+  inputLabel: {
+    paddingTop: 20,
+    paddingLeft: 10,
+    fontSize: 14,
+    fontWeight: '600',
+    fontStyle: 'normal',
+    color: `${colors.gray}`,
+  },
+  buttonContainer: {
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkbox: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  checkboxBackground: {
+    borderColor: `${colors.inputBorder}`,
+    borderWidth: 1,
+    margin: 7,
+    borderRadius: 100,
+  },
+  forgotPass: {
+    display: 'flex',
+    alignSelf: 'flex-end',
+    margin: 15,
+    color: `${colors.orange}`,
+  },
+  registrationContainer: {
+    margin: 30,
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  priorityContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  radioContainer: {
+    alignItems: 'center',
+  },
+  informalText: {
+    fontSize: 14,
+    fontWeight: '600',
+    fontStyle: 'normal',
+    color: `${colors.gray}`,
+  },
+  discardChanges: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    letterSpacing: 0,
+    textAlign: 'center',
+    color: `${colors.gray}`,
+  },
+});
