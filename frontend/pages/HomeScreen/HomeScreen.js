73bec0c8d36601257f8376ac46c896c5f2163098
Ahmet Işık <48672998+AhmetIsk@users.noreply.github.com>
2022-05-06 17:40:18 +0300

    implement user data in home screen

--- frontend/pages/HomeScreen/HomeScreen.js
+++ frontend/pages/HomeScreen/HomeScreen.js
@@ -1,24 +1,30 @@
 /* eslint-disable global-require */
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { Text, KeyboardAvoidingView, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, KeyboardAvoidingView, View, Image } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import { useIsFocused } from '@react-navigation/native';
 import { ButtonContainer } from '../../components/Containers';
 import SVGImg from '../../assets/logo.svg';
 import { styles } from './styles';
-import { userInfoToken } from '../../store/slices/user';
+import { userService } from '../../services/userService';
+import { userToken } from '../../store/slices/token';
 
 const HomeScreen = ({ navigation }) => {
-  const dispatch = useDispatch();
-  const userInfo = useSelector(userInfoToken);
-  console.log('This is user info', userInfo);
+  const isFocused = useIsFocused();
+  const [userData, setUserData] = useState(null);
+  const token = useSelector(userToken);
+  useEffect(() => {
+    userService.getUserData(token).then((res) => setUserData(res));
+  }, [isFocused, token])
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.header}>
         <Text style={styles.headerTitle}>ShopCart</Text>
       </View>
       <SVGImg width={118} height={134} />
-      <Text style={styles.welcomePage}>Welcome, {userInfo?.first_name} !</Text>
+      <Text style={styles.welcomePage}>Welcome, {userData?.user.first_name} !</Text>
+      <Image source={require('../../assets/homeScreenDesign.jpg')} style={{ height: 390, width: 385, top: 80 }} />
       <ButtonContainer />
     </KeyboardAvoidingView>
   );
