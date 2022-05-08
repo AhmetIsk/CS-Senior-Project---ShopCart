a941c3a44839a70bf819878a53f7f0462dcfc829
Ahmet Işık <48672998+AhmetIsk@users.noreply.github.com>
2022-05-09 00:08:26 +0300

    add delete option to comm button

--- frontend/components/Buttons/CommunityButton.js
+++ frontend/components/Buttons/CommunityButton.js
@@ -2,26 +2,65 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons'
+import { Swipeable } from 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
 import { colors } from '../../constants/styles'
+import { userService } from '../../services/userService';
+import { userToken } from '../../store/slices/token';
 
-export default function CommunityButton({ name, users, ownerName }) {
+export default function CommunityButton({ name, users, ownerName, navigation, rerender, setRerender, id }) {
+    const token = useSelector(userToken);
     const usersNames = [];
-    return (
-        <TouchableOpacity style={styles.buttonContainer} onPress={() => console.log('hello guys')}>
-            <View style={styles.infoContainer}>
-                <Text style={styles.communityName}>{name}</Text>
-                <Text style={styles.communityName}>{ownerName}</Text>
-            </View>
-            <View style={styles.people}>
-                <Ionicons
-                    name="people"
-                    size={25}
-                    color={colors.white}
-                    style={{ paddingLeft: 15, paddingRight: 15 }}
-                />
-                <Text>{users.map((user) => [...usersNames, user])}</Text>
-            </View>
+    const rightSwipeActions = () => (
+        <TouchableOpacity
+            style={{
+                backgroundColor: `${colors.headerRed}`,
+                justifyContent: 'center',
+                alignItems: 'flex-end',
+                right: -40,
+                height: 175,
+                alignSelf: 'center',
+                borderRadius: 12,
+                top: 20,
+                left: 0,
+            }}
+            onPress={() => userService.deleteShoplist(id, token).then(() => {
+                setRerender(!rerender);
+                navigation.navigate('Communities');
+            })}
+        >
+            <Text
+                style={{
+                    color: `${colors.white}`,
+                    paddingHorizontal: 10,
+                    fontWeight: '600',
+                    paddingVertical: 20,
+                }}
+            >
+                Delete
+            </Text>
         </TouchableOpacity>
+    );
+    return (
+        <Swipeable
+            renderRightActions={rightSwipeActions}
+        >
+            <TouchableOpacity style={styles.buttonContainer} onPress={() => console.log('hello guys')}>
+                <View style={styles.infoContainer}>
+                    <Text style={styles.communityName}>{name}</Text>
+                    <Text style={styles.communityName}>{ownerName}</Text>
+                </View>
+                <View style={styles.people}>
+                    <Ionicons
+                        name="people"
+                        size={25}
+                        color={colors.white}
+                        style={{ paddingLeft: 15, paddingRight: 15 }}
+                    />
+                    <Text style={styles.communityName}>{users.map((user) => [...usersNames, user.first_name])}</Text>
+                </View>
+            </TouchableOpacity>
+        </Swipeable>
     )
 }
 
