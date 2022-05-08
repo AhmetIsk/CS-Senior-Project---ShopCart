ebba34dd62a15d53c393abaafa184ba440592873
Ahmet Işık <48672998+AhmetIsk@users.noreply.github.com>
2022-05-09 00:13:50 +0300

    combine empty and my lists screens

--- frontend/pages/MyListsScreen/MyListsNavigator.js
+++ frontend/pages/MyListsScreen/MyListsNavigator.js
@@ -1,10 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { userToken } from '../../store/slices/token';
-import { userService } from '../../services/userService';
-import EmptyListsScreen from './EmptyListsScreen';
+import React from 'react';
 import MyListsScreen from './MyListsScreen';
 import AddShoppingListScreen from './AddShoppingListScreen';
 import ShoppingListScreen from '../ShoppingListScreen/ShoppingListScreen';
@@ -13,51 +9,35 @@ import BestPricesScreen from '../BestPricesScreen/BestPricesScreen';
 
 const Stack = createNativeStackNavigator();
 
-const MyListsNavigator = () => {
-  const token = useSelector(userToken);
-  const [isEmpty, setIsEmpty] = useState(true);
-  const ListProducts = () => {
-    userService.getUsersShoppingCartLists(token).then((products) => {
-      if (products.length > 0) setIsEmpty(false);
-    });
-  };
-  useEffect(() => {
-    ListProducts();
-  }, []);
-  // console.log('This is user info', userInfo.first_name);
-  return (
-    <Stack.Navigator>
-      {isEmpty ? (
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Empty Lists"
-          component={EmptyListsScreen}
-        />
-      ) : (
-        <Stack.Screen options={{ headerShown: false }} name="My Lists" component={MyListsScreen} />
-      )}
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="Add a Shopping List"
-        component={AddShoppingListScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="Shopping List"
-        component={ShoppingListScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="Add New Product"
-        component={AddNewProductScreen}
-      />
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="Best Prices"
-        component={BestPricesScreen}
-      />
-    </Stack.Navigator>
-  );
-};
+const MyListsNavigator = ({ navigation }) => (
+  <Stack.Navigator>
+    <Stack.Screen options={{ headerShown: false }} name="My Lists" component={MyListsScreen} />
+    <Stack.Screen
+      options={{ headerShown: false }}
+      name="Add a Shopping List"
+      component={AddShoppingListScreen}
+    />
+    <Stack.Screen
+      options={{ headerShown: false }}
+      name="Shopping List"
+      component={ShoppingListScreen}
+    />
+    <Stack.Screen
+      options={{ headerShown: false }}
+      name="Add New Product"
+      component={AddNewProductScreen}
+    />
+    <Stack.Screen
+      options={{ headerShown: false }}
+      name="Best Prices"
+      component={BestPricesScreen}
+    />
+    {/* <Stack.Screen
+            options={{ headerShown: false }}
+            name="Empty List"
+            component={EmptyListsScreen}
+          /> */}
+  </Stack.Navigator >
+);
 
 export default MyListsNavigator;
--- frontend/pages/MyListsScreen/MyListsScreen.js
+++ frontend/pages/MyListsScreen/MyListsScreen.js
@@ -3,11 +3,12 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ActivityIndicator, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { Entypo } from '@expo/vector-icons';
 import { useIsFocused } from '@react-navigation/native';
 import { userService } from '../../services/userService';
+import NOLists from '../../assets/noLists.svg';
 import { userToken } from '../../store/slices/token';
 import { colors } from '../../constants/styles';
 import { styles } from './styles/index';
@@ -21,32 +22,55 @@ const MyListsScreen = ({ navigation }) => {
   const [shopLists, setShopLists] = useState([]);
   const dispatch = useDispatch();
   const [rerender, setRerender] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   dispatch(resetId);
   const ListShopLists = () => {
     userService.getUsersShoppingCartLists(token).then((products) => {
-      const mappedItems = products.map((product) => (
-        <ShoppingListContainer
-          key={product.id}
-          id={product.id}
-          priority={product.priority}
-          name={product.name}
-          communities={product.communities}
-          navigation={navigation}
-          totalItems={product.products
-            .map((object) => object.quantity)
-            .reduce((prev, curr) => prev + curr, 0)}
-          rerender={rerender}
-          setRerender={setRerender}
-        />
-      ));
-      setShopLists(mappedItems);
+      if (products.length === 0) {
+        setShopLists(
+          <>
+            <NOLists style={{ marginTop: 220 }} />
+            <Text style={styles.emptyListHeader}>No Items in the Cart</Text>
+            <Text style={styles.emptyListText}>Create list and add products to the list.</Text>
+            <TouchableOpacity
+              style={styles.addListButton}
+              onPress={() => navigation.navigate('Add a Shopping List')}
+            >
+              <Text style={styles.addListLabel}>Add List</Text>
+            </TouchableOpacity>
+          </>
+        );
+        setIsEmpty(true);
+      }
+      else {
+        const mappedItems = products.map((product) => (
+          <ShoppingListContainer
+            key={product.id}
+            id={product.id}
+            priority={product.priority}
+            name={product.name}
+            communities={product.communities}
+            navigation={navigation}
+            totalItems={product.products
+              .map((object) => object.quantity)
+              .reduce((prev, curr) => prev + curr, 0)}
+            rerender={rerender}
+            setRerender={setRerender}
+          />
+        ));
+        setShopLists(mappedItems);
+        setIsEmpty(false);
+      }
+      setIsLoading(false);
     });
   };
   useEffect(() => {
+    setIsLoading(true);
     ListShopLists();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isFocused, rerender]);
-  console.log('this page shows shopping lists');
+  console.log('this page shows shopping lists', isLoading);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -60,8 +84,21 @@ const MyListsScreen = ({ navigation }) => {
         <Text style={styles.headerTitle}>My Lists</Text>
         <Text style={{ width: 33 }} />
       </View>
-      <ScrollView style={styles.scrollView}>{shopLists}</ScrollView>
-      <AddShopListButton onPress={() => navigation.navigate('Add a Shopping List')} />
+      {isLoading ? (
+        <ActivityIndicator
+          // visibility of Overlay Loading Spinner
+          visible={isLoading}
+          size="large" color={colors.orange}
+        />) : (
+        <>
+          {
+            isEmpty ?
+              shopLists :
+              <ScrollView style={styles.scrollView}>{shopLists}</ScrollView>
+          }
+          < AddShopListButton onPress={() => navigation.navigate('Add a Shopping List')} />
+        </>
+      )}
     </View>
   );
 };
